902481d6e636b4f41aa35c0fa045695246be04a9
Z_xun <1336791371@qq.com>
2023-06-16 09:34:50 +0800

    新建博客前端项目文件夹

---
+++ blogfront/src/test/java/com/example/blogfront/BlogfrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blogfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogfrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
