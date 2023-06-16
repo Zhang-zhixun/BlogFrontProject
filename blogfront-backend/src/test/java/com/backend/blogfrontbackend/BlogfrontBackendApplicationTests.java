df549eb00abf88dc930bad3356124c1d49d64663
Z_xun <1336791371@qq.com>
2023-06-16 21:17:39 +0800

    初始化博客前后端分离项目

---
+++ blogfront-backend/src/test/java/com/backend/blogfrontbackend/BlogfrontBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.blogfrontbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogfrontBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
